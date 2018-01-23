484759b2e85ad182a4d8394f3b907666806f9ed0
Kimihiro11 <cjassassinli1125@gmail.com>
2018-01-23 16:33:59 +0800

    小实验

---
+++ Javascritp_图片库/js/exmaple.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    var testdiv = document.getElementById("testdiv");
+    //alert(testdiv.innerHTML);
+    testdiv.innerHTML = " <p>This is <em>my</em> content.</p>";
+}
