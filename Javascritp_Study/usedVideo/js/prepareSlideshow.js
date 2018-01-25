981354e32657371493b05102d670d221b4af9f49
Kimihiro11 <cjassassinli1125@gmail.com>
2018-01-25 22:38:36 +0800

    little

--- Javascritp_Study/usedVideo/js/prepareSlideshow.js
+++ Javascritp_Study/usedVideo/js/prepareSlideshow.js
@@ -1,19 +1,25 @@
 function prepareSlideshow(){
     //确保浏览器支持DOM方法
-    if (!document.getElementById ||document.getElementsByTagName) return false;
+    if (!document.getElementById || !document.getElementsByTagName) return false;
     //确保元素存在
-    if (!document.getElementById("linklist") || document.getElementById("preview")) return false;
+    if (!document.getElementById("linklist") || !document.getElementById("preview")) return false;
     //为图片应用样式
     var preview = document.getElementById("preview");
     preview.style.position = "absolute";
     preview.style.left = "0px";
     preview.style.top = "0px";
     //取得列表中的所有链接
-    var list = documen.getElementById("linklist");
+    var list = document.getElementById("linklist");
     var links = document.getElementsByTagName("a");
     //为mouseover事件添加动画效果
     links[0].onmouseover = function(){
-        moveElement
+        moveElement("preview",-300,0,10);
+    }
+    links[1].onmouseover = function(){
+        moveElement("preview",-600,0,10);
+    }
+    links[2].onmouseover = function(){
+        moveElement("preview",-900,0,10);
     }
 }
 
@@ -43,3 +49,5 @@ function moveElement(elementID,final_x,final_y,interval){
     var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
     movement = setTimeout(repeat,interval);
 }
+
+addLoadEvent(prepareSlideshow);
